33a0fd8085934f57a035e499d508a2a3c10faa85
Dan Kempton <kemptondan@gmail.com>
2020-06-16 17:26:01 -0400

    Adding a timer Promise for the animation steps

--- animation.js
+++ animation.js
@@ -1,5 +1,10 @@
 const square = document.querySelector('.square');
 
+function timer(time) {
+	return new Promise((resolve) => {
+		setTimeout(resolve, time);
+	});
+}
 function handleClick() {
 	console.log('Working');
 }
